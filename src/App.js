31038f8a0664fd530de1e7fb99f2469c1fffd12f
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2023-02-15 22:46:04 +0530

    update datasource

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import { ProTable } from '@ant-design/pro-components';
 import { ConfigProvider } from 'antd';
 import enUS from 'antd/locale/en_US';
-import { useRef } from 'react';
+import { useState } from 'react';
 import request from 'umi-request';
 import {columns} from './column_scheme'
 
 const App = () => {
-  const actionRef = useRef();
+  const [tableListDataSource, setTableListDataSource] = useState([
+    {
+      "id": 1,
+      "title": "Appointment",
+      "description": "With professor",
+      "dueDate": "2023-03-20",
+      "tags": ["delay", "academics"],
+      "status": "open"
+    },
+    {
+      "id": 2,
+      "title": "Class Assignment",
+      "description": "Computer Networks",
+      "dueDate": "2023-02-25",
+      "tags": ["urgent", "academics"],
+      "status": "done"
+    },
+    {
+      "id": 3,
+      "title": "Dev Assignment",
+      "description": "Frontend task",
+      "dueDate": "2023-02-16",
+      "tags": ["urgent", "development"],
+      "status": "working"
+    }
+  ]);
+
   return (
     <ConfigProvider locale={enUS}>
       <ProTable
         columns={columns}
-        actionRef={actionRef}
         cardBordered
-
-        request={async (params = {}, sort, filter) => {
-          console.log(sort, filter);
-  
-          return request('http://localhost:5005/tasks', {
-            params,
-          });
-        }}
 
         editable={{
           type: 'multiple',
         }}
-        columnsState={{
-          persistenceKey: 'pro-table-singe-demos',
-          persistenceType: 'localStorage',
-          onChange(value) {
-            console.log('value: ', value);
-          },
-        }}
+
         rowKey="id"
-        search={{
-          labelWidth: 'auto',
-        }}
         options={{
           setting: {
             listsHeight: 400,
@@ -43,9 +51,26 @@ const App = () => {
         }}
         pagination={{
           pageSize: 5,
-          onChange: (page) => console.log(page),
         }}
         dateFormatter="string"
+
+      request={async (params, sorter, filter) => {
+        console.log(params, sorter, filter);
+
+        return Promise.resolve({
+          data: tableListDataSource,
+          success: true,
+        });
+      }}
+      
+      toolbar={{
+        search: {
+          onSearch: (value) => {
+            alert(value);
+          },
+        },
+      }}
+      search={false}
       />
     </ConfigProvider>
   )
