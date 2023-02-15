71f93ffe7d49064ec191084797580839225296a5
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2023-02-15 14:52:23 +0530

    component: table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import { ProTable } from '@ant-design/pro-components';
+import { Button, ConfigProvider, Tag } from 'antd';
+import enUS from 'antd/locale/en_US';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <p>Todo app</p>
-    </div>
-  );
+    <ConfigProvider locale={enUS}>
+      <ProTable
+      />
+    </ConfigProvider>
+  )
 }
 
 export default App;
