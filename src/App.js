a275e85f1d42fed40c6f76a2f971f12a8eb655ed
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2023-02-17 16:58:05 +0530

    done add task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
+import { PlusOutlined } from '@ant-design/icons';
 import { ProTable } from '@ant-design/pro-components';
 import { Button, ConfigProvider } from 'antd';
 import enUS from 'antd/locale/en_US';
+import { useRef, useState } from 'react';
 import request from 'umi-request'
 import {columns} from './columns'
 
 const App = () => {
+  const actionRef = useRef();
+  const [newRowId, setNewRowId] = useState(null);
+
   return (
     <ConfigProvider locale={enUS}>
       <ProTable
         columns={columns}
+        actionRef={actionRef}
         cardBordered
 
         editable={{
           type: 'multiple',
           onSave: (key, row, _, __) => {
-            // console.log(key, row);
-            request.put(`http://localhost:5005/tasks/${row.id}`, {
-              data: row,
-            })
-            .catch(function(error) {
-              console.log(error);
-            });
+            console.log(key, row);
+            if(typeof row.tags === 'string')
+              row.tags = row.tags.split(',');
+
+            if(row.id === newRowId) {
+              setNewRowId(null);
+              request.post(`http://localhost:5005/tasks`, {
+                data: row,
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+            }
+            else {
+              request.put(`http://localhost:5005/tasks/${row.id}`, {
+                data: row,
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+            }
           },
           onDelete: (key, row, _, __) => {
             request.delete(`http://localhost:5005/tasks/${row.id}`, {
@@ -43,7 +63,7 @@ const App = () => {
           pageSize: 5,
         }}
         dateFormatter="string"
-
+        headerTitle="Todo? || NoTodo?"
         request={async (params = {}, sort, filter) => {
           // console.log(sort, filter);
           return request('http://localhost:5005/tasks', {
@@ -65,6 +85,22 @@ const App = () => {
             },
           },
         }}
+        toolBarRender={() => 
+            <Button
+            type="primary"
+            onClick={() => {
+              const nid = (Math.random() * 1000000).toFixed(0);
+              setNewRowId(nid)
+              actionRef.current?.addEditRecord?.({
+                id: nid,
+                title: 'Task',
+              });
+            }}
+            icon={<PlusOutlined />}
+          >
+            Add
+          </Button>
+        }
         search={false}
       />
     </ConfigProvider>
